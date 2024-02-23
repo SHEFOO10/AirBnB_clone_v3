9e2dd2a82ac9040de40c53bd5b5564b9dfcae310
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-23 23:32:21 +0200

    Add stats route but you have to modified it after merge the pull request

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models.engine.db_storage import classes
+# from models import storage
 
 @app_views.route("/status",
                  strict_slashes=False)
 def status():
     """ return the status of The API """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats",
+                 strict_slashes=False)
+def stats():
+    """ retrieves the number of each objects by type """
+    # to remove later
+    import random
+
+    stats = {}
+    for cls in classes.items():
+        # stats.update({cls[0], storage.count(cls[1])})
+        stats.update({cls[0]: random.randint(2, 20)})
+    return jsonify(stats)
