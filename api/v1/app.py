10090e33b15da0a1920214c5bdc5b2b53a73fcfa
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-23 23:42:48 +0200

    Handle 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """3. Status of your API"""
 
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from models import storage
 from api.v1.views import app_views
@@ -15,7 +15,14 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(error):
+    """ reload storage after every request """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """ Handle 404 Code """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
