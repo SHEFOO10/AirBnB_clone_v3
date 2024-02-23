9cc49aea7d80d6b03e6d56060d18c61f0613739c
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-23 23:30:49 +0200

    disable strict_slashes & Add doc to status route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-
-@app_views.route("/status")
+@app_views.route("/status",
+                 strict_slashes=False)
 def status():
+    """ return the status of The API """
     return jsonify({"status": "OK"})
