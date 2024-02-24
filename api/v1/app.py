57e182b87ab72954bb559511e0572048d0fa3800
Mariem Ehab <Mariem.ehab234@gmail.com>
2024-02-24 06:25:58 -0800

    Task12

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 port = int(os.environ.get('HBNB_API_PORT', 5000))
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 
