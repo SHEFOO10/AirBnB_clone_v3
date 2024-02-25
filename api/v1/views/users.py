de2240527b3beb34c6202b4feed974034b75f706
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-26 01:03:32 +0200

    You can now create new User

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -45,6 +45,20 @@ def delete_user(user_id):
         return jsonify({"error": "Internal Server Error"}), 500
 
 
+@app_views.route('/users', methods=['POST'],
+                 strict_slashes=False)
+def create_user():
+    """ Create User """
+    changes = request.get_json(silent=True, force=True)
+    if not changes:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'email' not in changes:
+        return jsonify({"error": "Missing email"}), 400
+    if 'password' not in changes:
+        return jsonify({"error": "Missing password"}), 400
+    user = User(**changes)
+    user.save()
+    return jsonify(user.to_dict()), 201
 
 
 @app_views.route('users/<user_id>', methods=['PUT'],
