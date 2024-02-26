c0946d2bb7d38d5532ffc2b33a171fa8f80c659c
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-26 21:01:01 +0200

    Replace jsonify with abort

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a view for Place objects that handles all default RESTFul API actions """
-from flask import request, jsonify
+from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.place import Place
 from models import storage
@@ -12,7 +12,7 @@ def get_places(city_id):
     """ Retrieves the list of all Place objects of a City """
     city = storage.get('City', city_id)
     if city is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     places = [place.to_dict() for place in city.places]
     return jsonify(places), 200
 
@@ -22,7 +22,7 @@ def get_place(place_id):
     """ Retrieves a Place object. """
     place = storage.get('Place', place_id)
     if place is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     return jsonify(place.to_dict()), 200
 
 
@@ -32,7 +32,7 @@ def delete_place(place_id):
     """ Deletes a Place object """
     place = storage.get('Place', place_id)
     if place is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     storage.delete(place)
     storage.save()
     return jsonify({}), 200
@@ -44,17 +44,17 @@ def create_place(city_id):
     """ Creates a Place """
     city = storage.get('City', city_id)
     if city is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     query_parameters = request.get_json(force=True, silent=True)
     if not query_parameters:
-        return jsonify({"error": "Not a JSON"}), 400
+        abort(400, "Not a JSON")
     if 'user_id' not in query_parameters:
-        return jsonify({"error": "Missing user_id"}), 400
+        abort(400, "Missing user_id")
     user = storage.get('User', query_parameters.get('user_id'))
     if user is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     if 'name' not in query_parameters:
-        return jsonify({"error": "Missing name"}), 400
+        abort(400, "Missing name")
     query_parameters.update({"city_id": city_id})
     new_place = Place(**query_parameters)
     new_place.save()
@@ -66,10 +66,10 @@ def update_place(place_id):
     """ Updates a Place object """
     place = storage.get('Place', place_id)
     if place is None:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
     changes = request.get_json(force=True, silent=True)
     if not changes:
-        return jsonify({"error": "Not a JSON"}), 400
+        abort(400, "Not a JSON")
     print(changes)
     for key, value in changes.items():
         if key in ['name',
